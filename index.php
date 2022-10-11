e2f4248f7ee256d2e81b9a8a6893fa9614981e32
Vinicius da Silva Nogueira <msn-dovinicius@hotmail.com>
2022-10-11 14:57:36 -0300

    Fazendo o sidebar

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">
+	<!--icones-->
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" type="text/css" rel="stylesheet">
+	<!--Font-->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inter+Tight:ital,wght@0,300;0,400;0,600;1,500&display=swap" rel="stylesheet">
+
+	<title>Twitch</title>
+</head>
+<body>
+
+	<header>
+		<i class="fa-brands fa-twitch logo display_in"></i>
+		<h3 class="display_in procurar">Procurar</h3>
+		<i class="fa-solid fa-ellipsis-vertical display_in menu"></i>
+
+		<div class="center_h">
+			<input type="text" name="" value="Buscar">
+			<button><i class="fa-solid fa-magnifying-glass"></i></button>
+		</div><!--center_h-->
+
+		<div class="right_h">
+			<i class="fa-solid fa-crown coroa"></i>
+			<button class="entrar_btn">Entrar</button>
+			<button class="cadastrar_btn">Cadastrar-se</button>
+			<i class="fa-regular fa-user usuario"></i>
+		</div><!--right_h-->
+	</header>
+
+	<section class="sidebar">
+		<h3 class="titulo_sidebar display_in">CANAIS RECOMENDADOS</h3>
+		<i class="fa-solid fa-arrow-left display_in fechar_side"></i>
+
+		<div class="perfil_side">
+			<div class="img_perfil display_in"></div>
+			<div class="display_in">
+			<p class="nome_perfil">Usuário</p>
+			<p class="jogo_perfil">JOGO</p>
+			</div>
+			<div class="ao_vivo display_in"></div>
+			<p class="display_in">7,7 mil</p>
+		</div><!--perfil_side-->
+	</section>
+
+</body>
+</html>
